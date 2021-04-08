136312cf7fe7980e9fcc397f29f5820448c161ff
Roberto Giacomelli <giaconet.mailbox@gmail.com>
2021-04-08 19:44:03 +0200

    refactor `sourcecode` sub-package and apply mods

--- sourcecode.lua
+++ sourcecode.lua
@@ -2,44 +2,48 @@
 
 -- aux functions
 
-local function sendlua_to_latexenv(buf, start, stop, prt)
-    assert(stop >= start)
-    prt([=[\begin{lstlisting}[style=lua]]=])
-    for l = start, stop do
-        prt(buf[l])
+local function sendcode_to_latexenv(buf, stop, style, tcb, prt)
+    assert(stop > 0)
+    local tcb_s
+    if tcb then
+        tcb_s = ",numbers=none,xleftmargin=0pt,aboveskip=0pt,belowskip=0pt"
+    else
+        tcb_s = ""
     end
+    local begin = string.format([=[\begin{lstlisting}[style=%s%s]]=], style, tcb_s)
+    prt(begin)
+    prt(table.concat(buf, string.char(13), 1, stop))
     prt([[\end{lstlisting}]])
 end
 
-local function sendterminal_to_latexenv(buf, prompt, prt)
-    assert(#buf > 0)
-    prt([=[\vspace*{-1.5ex}\begin{lstlisting}[style=out,numbers=none]]=])
-    for _, l in ipairs(buf) do
-        prt(prompt .. l)
-    end
+local function sendterminal_to_latexenv(term_buf, prt)
+    if not term_buf then return end
+    prt([=[\vspace*{-1.5ex}\begin{lstlisting}[style=sourcecodeterm,numbers=none]]=])
+    prt(table.concat(term_buf))
     prt([[\end{lstlisting}]])
 end
 
-local function run_buffer(buf, last)
+local function run_file(filename, prompt)
+    local f = assert(io.popen("texlua "..filename))
+    local l = {}
+    local i = 0
+    for line in f:lines() do
+        i = i + 1
+        l[#l+1] = prompt
+        l[#l+1] = line
+        l[#l+1] = string.char(13)
+    end
+    if i > 0 then
+        return l
+    end
+end
+
+local function run_buffer(buf, last, prompt)
     local filename = "tmp.lua"
     local flua = io.open(filename, "w")
     flua:write(table.concat(buf, "\n", 1, last))
     flua:close()
-    local f = io.popen("texlua "..filename)
-    local l = {}
-    for line in f:lines() do
-        l[#l+1] = line
-    end
-    return l
-end
-
-local function run_file(filename)
-    local f = assert(io.popen("texlua "..filename))
-    local l = {}
-    for line in f:lines() do
-        l[#l+1] = line
-    end
-    return l
+    return run_file(filename, prompt)
 end
 
 local function getkey(line)
@@ -90,16 +94,28 @@ local function get_lines(buf, filename, delim_key)
     end
 end
 
+local function extract_ext(path)
+    local e = path:match "%.(%a+)$"
+    return e:lower()
+end
 
+-- library table
 local sourcecode = {
     prompt = "> ",
     _buffer = {},
     _counter = 0,
     _opt_key = {
-        file   = "_file",
-        run    = "_run",
-        select = "_sel",
-    }
+        file      = "_file",
+        run       = "_run",
+        select    = "_sel",
+        tex       = "_tex",
+        lua       = "_lua",
+        tcolorbox = "_tcb",
+    },
+    _ext_style = {
+        tex = "sourcecodelatex",
+        lua = "sourcecodelua",
+    },
 }
 
 -- luatex callback function 'process_input_buffer'
@@ -107,25 +123,34 @@ function sourcecode.process_input_buffer(line)
     if line:match [[\end{lines}]] then
         return line
     else
-        if not (line == '' and sourcecode._counter == 0) then
-            sourcecode._counter = sourcecode._counter + 1
-            local buf = sourcecode._buffer
-            buf[sourcecode._counter] = line
-	    end
-        return ''
-    end
-end
-
-function sourcecode.process_input_buffer_star(line)
-    if line:match [[\end{linesrun}]] then
-        return line
-    else
-        if not (line == '' and sourcecode._counter == 0) then
-            sourcecode._counter = sourcecode._counter + 1
-            local buf = sourcecode._buffer
-            buf[sourcecode._counter] = line
-	    end
-        return ''
+        if sourcecode._counter == 0 then
+            -- attribute processing (partial impl of ECMA-335)
+            local attr = line:match "%s*#%[%s*(.-)%s*%]"
+            if attr then
+                local field = sourcecode._opt_key[attr]
+                if field then
+                    assert(
+                        not sourcecode[field],
+                        "Duplicate attribute entry '"..attr.."'"
+                    )
+                    sourcecode[field] = true
+                else
+                    error("Attribute '"..attr.."' not found")
+                end
+                return ""
+            end
+            -- initial blank lines
+            if line == "" then
+                return ""
+            end
+            sourcecode._counter = 1
+            sourcecode._buffer[1] = line
+            return ""
+        end
+        -- common line  of code
+        sourcecode._counter = sourcecode._counter + 1
+        sourcecode._buffer[sourcecode._counter] = line
+        return ""
     end
 end
 
@@ -136,6 +161,9 @@ function sourcecode:reset()
     self._file = nil
     self._run = nil
     self._sel = nil
+    self._tex = nil
+    self._lua = nil
+    self._tcb = nil
     return self
 end
 
@@ -174,26 +202,51 @@ function sourcecode:load_file()
     return self
 end
 
--- file
--- select
--- run
 -- lines of code comes from a source file
 -- lines of code comes from the luatex callback
+-- option:
+-- file        = "_file",
+-- run         = "_run",
+-- select      = "_sel",
+-- tex         = "_tex",
+-- lua         = "_lua",
+-- tcolorbox = "_tcb",
 function sourcecode:typeset()
     local buffer = self._buffer
     local counter = self._counter
     assert(counter > 0, "Empty buffer. Maybe you forget to run load_file()")
     local prt = self._tkprint
-    sendlua_to_latexenv(buffer, 1, counter, prt)
-    if self._run then
-        local filename = self._file
-        if filename and (not self._sel) then
-            local term_output = run_file(filename)
-            sendterminal_to_latexenv(term_output, self.prompt, prt)
+    local filename = self._file
+    -- code style
+    local ext
+    local lua, tex = self._lua, self._tex
+    if filename then
+        assert( (not lua) and (not tex) )
+        ext = extract_ext(filename)
+        if ext == "sty" then ext = "tex" end
+    else
+        if lua then
+            assert(not tex)
+            ext = "lua"
+        elseif tex then
+            assert(not lua)
+            ext = "tex"
         else
-            local term_output = run_buffer(buffer, counter)
-            sendterminal_to_latexenv(term_output, self.prompt, prt)
+            ext = "lua"
         end
+    end
+    local code_style = assert(self._ext_style[ext])
+    -- we are inside a tcolorbox?
+    local tcb = self._tcb
+    sendcode_to_latexenv(buffer, counter, code_style, tcb, prt)
+    if self._run then
+        local term_output
+        if filename and (not self._sel) then
+            term_output = run_file(filename, self.prompt)
+        else
+            term_output = run_buffer(buffer, counter, self.prompt)
+        end
+        sendterminal_to_latexenv(term_output, prt)
     end
     self:reset()
 end
